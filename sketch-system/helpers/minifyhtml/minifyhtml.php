f3df7d078bd61d437a53430df68dd49bf3943b2d
Kevin Dibble <digitalskill@xtra.co.nz>
2012-07-22 19:27:41 +1200

    General Updates
    Better for support for no .htaccess files
    
    Signed-off-by: Kevin Dibble <digitalskill@xtra.co.nz>

--- sketch-system/helpers/minifyhtml/minifyhtml.php
+++ sketch-system/helpers/minifyhtml/minifyhtml.php
@@ -7,5 +7,13 @@ function compress_page($buffer){
 		'/\s(![^\r\n])\s+(![^\r\n])/'=>' ',
 		'/&(?![A-Za-z0-9#]{1,7};)/'=>'&amp;'
 	);
-	return str_replace(array(';###;','href="/',"href='/","src='/",'src="/'),array('&&','href="'.urlPath(),"href='".urlPath(),"src='".urlPath(),'src="'.urlPath()),preg_replace(array_keys($search), array_values($search),str_replace("&&",';###;',$buffer)));
+	$path = urlPath();
+	if(!getSettings("htaccess")){
+		$path = str_replace("index.php","",$path);
+		$buffer = str_replace(
+							array("index.php/sketch-images/","index.php/sketch-files",'"sketch-images',"'sketch-images",'"sketch-files',"'sketch-files"),
+							array($path."/sketch-images",$path."/sketch-files",'"'.$path.'/sketch-images',"'".$path."/sketch-images",'"'.$path.'/sketch-files',"'".$path."/sketch-files"),
+							$buffer);
+	}
+	return str_replace(array(';###;','href="/',"href='/","src='/",'src="/'),array('&&','href="'.$path,"href='".$path,"src='".$path,'src="'.$path),preg_replace(array_keys($search), array_values($search),str_replace("&&",';###;',$buffer)));
 }
