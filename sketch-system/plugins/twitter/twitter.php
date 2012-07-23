3c31f0090107e4b477b00fd227d92c5133304762
Kevin Dibble <digitalskill@xtra.co.nz>
2012-07-23 18:05:40 +1200

    Social Icons Updated
    Signed-off-by: Kevin Dibble <digitalskill@xtra.co.nz>

--- sketch-system/plugins/twitter/twitter.php
+++ sketch-system/plugins/twitter/twitter.php
@@ -13,28 +13,7 @@ class TWITTER extends PLUGIN {
 	}
 	function display($args=''){				// [ REQUIRED ]
 		if(isset($args['links'])){
-			?>
-            <div id="socials">
-                <ul>
-                  <li><a href="<?php echo urlPath("newsfeed"); ?>"><img src="sketch-images/icon-rss.png" alt="" /></a></li>
-                  <?php if($this->e('screen_name')!=''){?>
-                  <li><a href="https://twitter.com/#!/<?php echo $this->e('screen_name'); ?>"><img src="sketch-images/icon-twitter.png" alt="" /></a></li>
-                  <?php } ?>
-                   <?php if($this->e('dribble_name')!=''){?>
-                  <li><a href="http://dribbble.com/<?php echo $this->e('dribble_name'); ?>"><img src="sketch-images/icon-dribble.png" alt="" /></a></li> 
-                  <?php } ?>
-                   <?php if($this->e('tumblr_name')!=''){?>
-                  <li><a href="#"><img src="sketch-images/icon-tumblr.png" alt="" /></a></li>
-                   <?php } ?>
-                   <?php if($this->e('flicker_name')!=''){?>
-                  <li><a href="#"><img src="sketch-images/icon-flickr.png" alt="" /></a></li>
-                   <?php } ?>
-                   <?php if($this->e('facebook_name')!=''){?>
-                  <li><a href="http://www.facebook.com/<?php echo $this->e('facebook_name'); ?>"><img src="sketch-images/icon-facebook.png" alt="" /></a></li>
-                   <?php } ?>
-                </ul>
-              </div>
-      <?php
+			@include(loadView("twitteroutput",false,true));
 		}else{
 			if(isset($args['timeline'])){
 				$this->url = str_replace("#",$this->e("screen_name"),$args['url']);
