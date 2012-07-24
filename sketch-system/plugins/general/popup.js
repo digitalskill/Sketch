df6786abd2d64460de3c1cbfc8bdb7ba5c064517
Kevin Dibble <digitalskill@xtra.co.nz>
2012-07-24 13:44:13 +1200

    Popup and lightbox update
    Signed-off-by: Kevin Dibble <digitalskill@xtra.co.nz>

--- sketch-system/plugins/general/popup.js
+++ sketch-system/plugins/general/popup.js
@@ -22,7 +22,8 @@ var Popup = new Class({
 		parent		: false,
 		iframe		: false,
 		zindex		: 299,
-		className	: ''
+		className	: '',
+		caption		: ''
 	},
  	initialize : function(obj,options){
 		this.setOptions(options);
@@ -42,6 +43,7 @@ var Popup = new Class({
 				this.setOptions(JSON.decode("{"+this.setupOps+"}"));
 			}
 		}
+		this.options.caption = $(this.object).get('title');
 		if(this.options.url){
 		    this.options.url = this.options.url + ((this.options.url.contains('?'))? '&ajax=ajax'  : '?ajax=ajax');
 		}
@@ -50,16 +52,12 @@ var Popup = new Class({
 	centerPopup : function(){
 		if($(this.masterContainer)){
 			$(this.mask).setStyles({"height":0,"width":0});
-			$(this.mask).setStyles({"height":$(window).getSize().y,"width":$(window).getSize().x,"position":"fixed"});	
-			$(this.masterContainer).position({relativeTo:$(this.mask),"position":"center"});
-			if($(this.masterContainer).getStyle("top") < 0){
-				$(this.masterContainer).setStyle("top",10);
-			}
+			$(this.mask).setStyles({"height":$(window).getSize().y,"width":$(window).getSize().x,"position":"fixed"});
 			$(this.surround).setStyles({"height":$(this.mask).getSize().y});
-			if($(this.closeButton)){
-				$(this.closeButton).setStyles({"margin-top":0,"margin-left":0});
-				$(this.closeButton).position({relativeTo:$(this.masterContainer),"position":"topRight","edge":"center"});
-				$(this.closeButton).setStyles({"margin-top":this.options.closeOffsetT,"margin-left":this.options.closeOffsetL});
+			var h = $(window).getSize().y/2 - this.options.height/2 > 0 ? $(window).getSize().y/2 - this.options.height/2 : 20;
+			$(this.masterContainer).morph({"margin-top":h});
+			if($('popupSpinner')){
+				$('popupSpinner').morph({"top":$(window).getSize().y/2 - this.options.height/2});
 			}
 		}
 	},
@@ -69,6 +67,11 @@ var Popup = new Class({
 	    $(this.masterContainer).fade("in");
 	     $(this.masterContainer).unspin();
 	    this.centerPopup();
+	},
+	testclose: function(event){
+		if(event && (event.target.hasClass("popup-background") || event.target.hasClass("popup-mask") )){
+			this.closePopup();
+		}
 	},
 	closePopup: function(){
 		if($(this.mask)){
@@ -112,8 +115,11 @@ var Popup = new Class({
 		this.mask = new Mask($(document.body),{useIframeShim:true,hideOnClick:false,destroyOnHide:true,maskMargins:true});
 		$(this.mask).setStyles({"z-index":this.options.zindex});
 		$(this.mask).addClass(this.options.className);
+		$(this.mask).addClass("popup-mask");
 		this.mask.show();
 		$(this.mask).pin();
+		
+		$(this.mask).addEvent("click",this.testclose.bind(this));
 
 		// hide the body
 		this.surround = new Element("div",{"class":"popup-background"});
@@ -122,29 +128,37 @@ var Popup = new Class({
 		$(this.surround).setStyles({"height":$(this.mask).getSize().y});
 		
 		// Add the popup Container
-		this.masterContainer = new Element("div",{"class":"master-container","id":"master-container","overflow":"hidden","styles":{"width":30,"height":30}});
-		this.masterContainer.setOpacity(0.01);
+		this.masterContainer = new Element("div",{"class":"master-container","id":"master-container","overflow":"hidden","styles":{"margin-top":$(window).getSize().y/2 - this.options.height/2,"width":30,"height":30}});
+		this.masterContainer.setOpacity(0);
 		$(this.masterContainer).inject($(this.mask),"top");
-		$(this.masterContainer).position({relativeTo:$(this.mask),"position":"center"});
 		$(this.masterContainer).set("morph",{onComplete:this.resetPopup.bind(this)});
-
+		
+		// ADD the content holder
+		this.contentHolder = new Element("div",{'styles':{'position':'relative','overflow':'hidden','width':'100%','height':'100%','z-index':0}});
+		$(this.contentHolder).inject($(this.masterContainer),"top");
+		
 		// Add the loader animation
 		$(this.masterContainer).set("spinner",{"id":"popupSpinner"});
 		$(this.masterContainer).spin();
-		$('popupSpinner').setStyles({"margin-left":this.options.maskOffsetL,"margin-top":this.options.maskOffsetT});
 
 		// Create close button
 		this.closeButton 	= new Element("div",{"class":"popup-closeBtn png"});
 		$(this.closeButton).setOpacity(0);
-		$(this.closeButton).inject($(this.mask),"top");
+		$(this.closeButton).inject($(this.masterContainer),"top");
+		$(this.closeButton).setStyles({"top":-23,"right":-25});
+		$(this.masterContainer).setStyle("overflow","visible");
 		$(this.closeButton).addEvent("click",this.closePopup.bind(this));
-
+		
+		this.caption = new Element("div",{'styles':{'width':'100%','position':'absolute','height':'auto','top':'auto','bottom':'-25','left':0}});
+		$(this.caption).inject($(this.masterContainer),'bottom');
+		$(this.caption).addClass("pop-caption");
+		
 		// Load content
 		if($(this.options.id)){
-		    $(this.masterContainer).set("html",$(this.options.id).get("html"));
-		    if($(this.masterContainer).getElement("img")){
+		    $(this.contentHolder).set("html",$(this.options.id).get("html"));
+		    if($(this.contentHolder).getElement("img")){
 			this.images = [];
-			$(this.masterContainer).getElements("img").each(function(item,index){
+			$(this.contentHolder).getElements("img").each(function(item,index){
 			   this.images.push($(item).get("src"));
 			},this);
 			new Asset.images(this.images,{onComplete:this.showContent.bind(this),onError:this.showContent.bind(this)});
@@ -155,128 +169,98 @@ var Popup = new Class({
 			if(this.options.iframe == true){
 				this.options.zindex = (this.options.zindex==299)? 999 : this.options.zindex;
 				$(this.mask).setStyle("z-index",this.options.zindex + " !important");
-				$(this.masterContainer).set("html","<iframe src='"+this.options.url+"' allowtransparency='1' frameborder='0' style='margin:5px' height='"+(this.options.height-10)+"' width='"+(this.options.width-10)+"' scrolling='auto'></iframe>");
+				$(this.contentHolder).set("html","<iframe src='"+this.options.url+"' allowtransparency='1' frameborder='0' style='margin:5px' height='"+(this.options.height-10)+"' width='"+(this.options.width-10)+"' scrolling='auto'></iframe>");
 				this.showContent();
 			}else{
 				if(this.options.iframe ==false && (this.options.url.contains("jpg") || this.options.url.contains("png") || this.options.url.contains("gif") || this.options.url.contains("JPG") || this.options.url.contains("PNG") || this.options.url.contains("GIF"))){
 					this.singleImage = new Asset.image(this.options.url,{onLoad:this.showContent.bind(this),onError:this.closePopup.bind(this),onAbort:this.closePopup.bind(this)});
 				}else{	
-					$(this.masterContainer).set("load",{onComplete: this.showContent.bind(this),method:'post'});
-					$(this.masterContainer).load(this.options.url);
+					$(this.contentHolder).set("load",{onComplete: this.showContent.bind(this),method:'post'});
+					$(this.contentHolder).load(this.options.url);
 				}
 			}
 			$(document.body).addEvent("closepop",this.closePopup.bind(this));
 		}
-		
-		/*
-		if($(this.options.id)){
-		    this.showSecondContent();
-		}else{
-		    $(this.masterContainer).spin();
-		    $(this.masterContainer).set("load",{onComplete: this.showSecondContent.bind(this)});
-		    $(this.masterContainer).load(this.options.url);
-		}
-		*/
-		
-		
-		// center the popups
-		this.centerPopup();
-
+			
 		// Keep the mask size accurate
 		$(document.body).addEvent("popsize",this.centerPopup.bind(this));
 		$(window).addEvent("resize",function(){$(document.body).fireEvent("popsize");});
-	    }
-	},
-	showSecondContent: function(){
-	    if($(this.masterContainer).getElement("img")){
-		this.images = [];
-		$(this.masterContainer).getElements("img").each(function(item,index){
-		    this.images.push($(item).get("src"));
-		},this);
-		new Asset.images(this.images,{onComplete:this.loadSecondContent.bind(this),onError:this.loadSecondContent.bind(this)});
-	    }else{
-		this.loadSecondContent();
-	    }
-	},
-	loadSecondContent:function(){
-	    var maskSize 	= $(window).getSize();
-	    var size = 0;
-	    // Get size of content
-	    $(this.masterContainer).setOpacity(0.5);
-	    var currentSize = $(this.masterContainer).getSize();
-	    $(this.masterContainer).setStyles({"height":"auto","width":"auto"});
-	    $(this.masterContainer).set("html",$(this.options.id).get("html"));
-	    $(this.masterContainer).getElements("img").setOpacity(0.01);
-	    if(this.options.auto==true){
-		if($(this.masterContainer).getElement(".sizeto")){
-		    size = $(this.masterContainer).getElement(".sizeto").measure(function(){
-			return this.getSize();
-		    });
-		}else{
-		    size = $(this.masterContainer).measure(function(){
-			return this.getSize();
-		    });
-		}
-		this.options.width = size.x;
-		this.options.height = size.y;
-	    }
-	    $(this.masterContainer).setStyles({"height":currentSize.y,"width":currentSize.x});
-	    if(this.options.maskWidth==0){
-		this.options.maskWidth = this.options.width;
-	    }
-	    if(this.options.maskHeight==0){
-		    this.options.maskHeight = this.options.height;
-	    }
-	    if($(this.masterContainer).getSize().y != this.options.height || $(this.masterContainer).getSize().x != this.options.width){
-		$(this.masterContainer).morph({"left":((maskSize.x-this.options.maskWidth)/2),"top":((maskSize.y-this.options.maskHeight)/2),"width":this.options.maskWidth,"height":this.options.maskHeight});
-	    }else{
-		$(this.masterContainer).morph({"opacity":0.5});
+		$(window).addEvent("scroll",function(){$(document.body).fireEvent("popsize");});
 	    }
 	},
 	setupNavArrows: function(){
 	    if($(this.options.parent)){
-		$(this.masterContainer).getElements(".navarrows").each(function(item,index){
-		    var me = this.navarrowclick.bind(this)
-		    $(item).addEvent("click",function(){me(this);});
-		},this);
+			if(!$(this.masterContainer).getElement(".navarrows")){
+				var pright = new Element("a",{'class':'popup-rightarrow'});
+				$(pright).inject($(this.masterContainer));
+				$(pright).addClass("navarrows");
+				$(pright).addClass("button");
+				$(pright).set("html","<span class='icons rightarrow'></span>");
+				
+				var pleft = new Element("a",{'class':'popup-leftarrow'});
+				$(pleft).inject($(this.masterContainer));
+				$(pleft).addClass("navarrows");
+				$(pleft).addClass("button");
+				$(pleft).set("html","<span class='icons leftarrow'></span>");
+				
+				$(pright).fade(0.3);
+				$(pleft).fade(0.3);
+				$(pright).addEvent("click",this.navarrowclick.bind(this));
+				$(pleft).addEvent("click",this.navarrowclick.bind(this));
+			}
 	    }
 	},
-	navarrowclick: function(self){
-	    var theIndex = 0;
+	navarrowclick: function(event){
+		if(event){
+			event.stop();	
+		}
+	    this.theIndex = 0;
+		var theimg   = null;
 	    $(this.options.parent).getElements(".popupActivated").each(function(item,index){
-		if($(item).hasClass("pop-current")){
-		    theIndex = index;
-		}
-	    },theIndex);
-	    if($(self).hasClass("popup-leftarrow")){
-		if($(this.options.parent).getElements(".popupActivated")[theIndex-1]){
-		    $(this.options.parent).getElements(".popupActivated")[theIndex-1].fireEvent("click");
-		}else{
-		    $(this.options.parent).getElements(".popupActivated").getLast().fireEvent("click");
-		}
+			if($(item).hasClass("pop-current")){
+				this.theIndex = index;
+			}
+	    },this);
+	    if($(event.target).hasClass("popup-leftarrow")){
+			if($(this.options.parent).getElements(".popupActivated")[this.theIndex-1]){
+				theimg = $(this.options.parent).getElements(".popupActivated")[this.theIndex-1];
+			}else{
+				theimg = $(this.options.parent).getElements(".popupActivated").getLast();
+			}
 	    }else{
-		if($(this.options.parent).getElements(".popupActivated")[theIndex+1]){
-		    $(this.options.parent).getElements(".popupActivated")[theIndex+1].fireEvent("click");
-		}else{
-		   $(this.options.parent).getElements(".popupActivated")[0].fireEvent("click");
-		}
+			if($(this.options.parent).getElements(".popupActivated")[this.theIndex+1]){
+				theimg = $(this.options.parent).getElements(".popupActivated")[this.theIndex+1];
+			}else{
+			   theimg = $(this.options.parent).getElements(".popupActivated")[0];
+			}
 	    }
+		if($(theimg).getElement("img")){
+			$(this.options.parent).getElements(".popupActivated").removeClass("pop-current");
+			$(theimg).addClass("pop-current");
+			this.options.caption = $(theimg).get("title");
+			thepath = $(theimg).getElement("img");
+			if(this.singleImage){
+				this.singleImage.destroy();
+			}
+			this.singleImage = new Asset.image($(thepath).get("src"),{onLoad:this.showContent.bind(this),onError:this.closePopup.bind(this),onAbort:this.closePopup.bind(this)});
+		}
 	},
 	showContent: function(){
 		var maskSize 	= $(window).getSize();
 		var size = 0;
 		
 		if(this.options.iframe ==false && (this.options.url.contains("jpg") || this.options.url.contains("png") || this.options.url.contains("gif") || this.options.url.contains("JPG") || this.options.url.contains("PNG") || this.options.url.contains("GIF"))){
-			this.singleImage.inject($(this.masterContainer));
+			this.singleImage.inject($(this.contentHolder));
 			this.options.width = this.singleImage.get("width");
 			this.options.height = this.singleImage.get("height");
 		}
-		
+
+		$(this.caption).set("html",this.options.caption);	
 		// Get size of content
 		if(this.options.auto==true){
 		    $(this.masterContainer).setStyles({"width":"auto","height":"auto","max-width":600,"max-height":600});
 		    if($(this.masterContainer).getElement(".sizeto")){
-			size = $(this.masterContainer).getElement(".sizeto").measure(function(){
+				size = $(this.masterContainer).getElement(".sizeto").measure(function(){
 			    return this.getSize();
 			});
 		    }else{
@@ -293,12 +277,14 @@ var Popup = new Class({
 		if(this.options.maskHeight==0){
 			this.options.maskHeight = this.options.height;
 		}
+		
+		$(this.masterContainer).morph({"width":this.options.width,"height":this.options.height});
 		$('popupSpinner').set("morph",{onComplete:this.removeAnimation.bind(this)});
 		$('popupSpinner').getElement(".spinner-content").setStyles({"top":"50%","left":"50%","margin-left":"-15px","margin-top":"-15px"});
-		$('popupSpinner').morph({"left":((maskSize.x-this.options.maskWidth)/2),"top":((maskSize.y-this.options.maskHeight)/2),"width":this.options.maskWidth,"height":this.options.maskHeight});
+		$('popupSpinner').morph({"left":((maskSize.x-this.options.maskWidth)/2),"top":$(this.masterContainer).getPosition().y,"width":this.options.maskWidth,"height":this.options.maskHeight});
 		var valform = this.validateForms.bind(this);
 		valform.delay(500);
-		this.centerPopup.bind(this).delay(2000);
+		this.centerPopup.bind(this).delay(500);
 	},
 	validateForms:function(){
 		$('master-container').getElements("form").each(function(item,index){
@@ -309,7 +295,6 @@ var Popup = new Class({
 		$(this.masterContainer).setOpacity(1);
 		var maskSize 	= $(window).getSize();
 		$(this.masterContainer).setStyles({"height":this.options.height+"px","width":this.options.width+"px"});
-		$(this.masterContainer).position();
 		$(this.masterContainer).addClass(this.options.classes);
 		this.showResult.bind(this).delay(500);
 	},
@@ -325,7 +310,6 @@ var Popup = new Class({
 		$(this.masterContainer).addEvent("mouseleave",this.hideInnerPop.bind(this));
 	    }
 	    this.setupNavArrows();
-	    $(this.closeButton).position({relativeTo:$(this.masterContainer),"position":"topRight","edge":"center"});
 	    $(this.closeButton).setStyles({"margin-top":this.options.closeOffsetT,"margin-left":this.options.closeOffsetL});
 	    $(this.closeButton).fade("in");
 	    
